2a9fe9ab3d808068ea9713abe74aa592c03c9319
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-01-28 23:37:50 +0200

    Add Suspense for post loading in Home component; improve user experience with loading fallback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import CreatePost from "@/components/CreatePost";
 import PostCard from "@/components/PostCard";
 import WhoToFollow from "@/components/WhoToFollow";
 import { currentUser } from "@clerk/nextjs/server";
+import { Suspense } from 'react';
 
 export default async function Home() {
   const user = await currentUser();
@@ -15,11 +16,13 @@ export default async function Home() {
       <div className="lg:col-span-6">
         {user ? <CreatePost /> : null}
 
-        <div className="space-y-6">
-          {posts.map((post) => (
-            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
-          ))}
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <div className="space-y-6">
+            {posts.map((post) => (
+              <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+            ))}
+          </div>
+        </Suspense>
       </div>
 
       <div className="hidden lg:block lg:col-span-4 sticky top-20">
